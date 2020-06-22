3835f4c8af8978d68177dd2ab330129224958876
CameronK24 <cskelly15@gmail.com>
2020-06-22 15:52:26 -0600

    finished all but black diamond cause I don't trust myself to not mess everything up when I try it.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
+      apiUrl: 'https://practiceapi.devmountain.com/api'
     };
 
     this.updatePost = this.updatePost.bind( this );
@@ -19,19 +21,38 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get(`${this.state.apiUrl}/posts`)
+    .then(res => {
+      // console.log(res.data);
+      this.setState({posts: res.data});
+    })
+    .catch(err => alert('Failed to get posts'))
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    // console.log(`id ${id} text ${text}`)
+    axios.put(`${this.state.apiUrl}/posts?id=${id}`, { text })
+    .then(res => {
+      this.setState({posts: res.data})
+    })
+    .catch(err => alert('Failed to update post'))
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`${this.state.apiUrl}/posts?id=${id}`)
+    .then(res => {
+      this.setState({posts: res.data})
+    })
+    .catch(err => 'Failed to delete post')
   }
 
-  createPost() {
-
+  createPost(text) {
+    // console.log(text);
+    axios.post(`${this.state.apiUrl}/posts`, {text})
+    .then(res => {
+      this.setState({posts: res.data})
+    })
+    .catch(err => 'Failed to create post')
   }
 
   render() {
@@ -43,7 +64,20 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={this.createPost} />
+
+          {
+            // console.log(posts),
+            posts.map(post => (
+              <Post key={post.id} 
+                    text={post.text} 
+                    date={post.date} 
+                    id={post.id}
+                    updatePostFn={this.updatePost} 
+                    deletePostFn={this.deletePost}
+                     />
+            ))
+          }
           
         </section>
       </div>
